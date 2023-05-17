437826f9fc0aaf09d399183525f8594b56ece8e9
Mohammad <moh.hossain98@gmail.com>
2023-05-17 16:55:35 -0400

    post crud done

--- controllers/posts.js
+++ controllers/posts.js
@@ -122,15 +122,44 @@ const createPost = async (req, res) => {
   const { id: userId } = req.user;
   // create the post
   console.log({ userId, caption });
-  const { data, error } = await supabase.from("post").insert([
-    {
-      user_id: userId,
-      caption,
-    },
-  ]);
-
+  const { data, error } = await supabase
+    .from("post")
+    .insert([
+      {
+        user_id: userId,
+        caption,
+      },
+    ])
+    .select(
+      `*, user:profiles(id, name, username, profile_url), likes: like(user_id), comments: comment(user_id), reposts: repost(user_id), content(*)`
+    );
   if (error) return res.status(500).json({ error: error.message });
-  res.status(201).json(data);
+
+  // console.log(data[0]);
+
+  // count the likes comments and resposts of the data
+  data[0].isLiked = false;
+  data[0].isReposted = false;
+
+  // check if the user has liked the post
+  data[0].likes.forEach((like) => {
+    if (like.user_id === req.user.id) {
+      data[0].isLiked = true;
+    }
+  });
+
+  // check if the user has reposted the post
+  data[0].reposts.forEach((repost) => {
+    if (repost.user_id === req.user.id) {
+      data[0].isReposted = true;
+    }
+  });
+
+  data[0].likes = data[0].likes.length;
+  data[0].comments = data[0].comments.length;
+  data[0].reposts = data[0].reposts.length;
+
+  res.json(data);
 };
 
 // PATCH /posts/:id
@@ -183,7 +212,7 @@ const updatePost = async (req, res) => {
 // DELETE /posts/:id
 const deletePost = async (req, res) => {
   const { id } = req.params;
-  const { data, error } = await supabase.from("posts").delete().eq("id", id);
+  const { data, error } = await supabase.from("post").delete().eq("id", id);
   if (error) return res.status(500).json({ error: error.message });
   res.json(data);
 };
