2208d7c601d64b46ae57f52ae643202dda4073a7
farhan haider <fhaider085@gmail.com>
2023-05-19 15:53:24 -0400

    feat(GET comments_like): comment likes returns isliked and likes count grouped by comment_id
    farhan

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,3 +1,4 @@
+import { log } from "console";
 import supabase from "../supabase/supabase.js";
 
 // GET /comment/:id => get all comments for a post
@@ -15,6 +16,48 @@ const getComments = async (req, res) => {
     console.log(error);
     return res.status(400).json({ error: error.message });
   }
+
+  const { data: likesCount, error: error2 } = await supabase.rpc(
+    "get_comment_likes",
+    {
+      commentid: data.map((comment) => comment.id),
+    }
+  );
+
+  if (error2) return res.status(400).json({ error: error2.message });
+
+  // map the likesCount and get the like_count. then add the like_count to the data after matching the id with comment_id
+  console.log(req.user.id);
+  data.forEach((comment) => {
+    comment.likes = 0;
+    likesCount.forEach((like) => {
+      if (like.comment_id === comment.id) {
+        comment.likes = like.like_count;
+      }
+    });
+  });
+
+  // get all comments that the user has liked from the comment_id
+  const { data: likedComments, error: error3 } = await supabase
+    .from("comment_likes")
+    .select("comment_id")
+    .in(
+      "comment_id",
+      likesCount.map((like) => like.comment_id)
+    )
+    .eq("user_id", req.user.id);
+  if (error3) return res.status(400).json({ error: error3.message });
+
+  // map the likedComments and get the comment_id. then add the isLiked to the data after matching the id with comment_id
+  data.forEach((comment) => {
+    comment.isLiked = false;
+    likedComments.forEach((like) => {
+      if (like.comment_id === comment.id) {
+        comment.isLiked = true;
+      }
+    });
+  });
+
   return res.status(200).json(data);
 };
 
@@ -57,3 +100,14 @@ const deleteComment = async (req, res) => {
 };
 
 export { createComment, getComments, deleteComment };
+
+// drop function if exists get_comment_likes;
+// create or replace function get_comment_likes(commentid int[])
+//   RETURNS TABLE (comment_id INTEGER, like_count INTEGER) AS
+// $$
+//   SELECT comment_id, COUNT(*) AS like_count
+//   FROM comment_likes
+//   WHERE comment_id = ANY(commentid)
+//   GROUP BY comment_id;
+// $$
+// LANGUAGE sql;
