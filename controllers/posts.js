9eba64b573bacc4055bd5c576c4e2a5d1a6e8b0f
farhan haider <fhaider085@gmail.com>
2023-06-13 10:23:27 -0400

    feat/image upload + compression + blurhash

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,9 @@
 import supabase from "../supabase/supabase.js";
 import extractHashtags from "../helpers/hashtags.js";
+import axios from "axios";
+import sharp from "sharp";
+import { encode } from "blurhash";
+import convert from "heic-convert";
 
 // this will get all the posts by the user
 const getUserPosts = async (req, res) => {
@@ -117,63 +121,166 @@ const getOnePost = async (req, res) => {
 
 // POST /posts
 const createPost = async (req, res) => {
-  // get the caption from the body
   const { caption } = req.body;
-  // array of dimensions of the media
-  // const dimensions = JSON.parse(req.body.dimensions);
+  let dimensions = [];
+  if (req.body.dimensions) dimensions = JSON.parse(req.body.dimensions);
   const { id: userId } = req.user;
-
-  console.log(caption);
-  // create the post
-
-  // the media files with buffer
   const { files: media } = req;
-  // console.log(dimensions);
-  console.log({ userId, caption });
-  console.log(media);
 
-  // create the post
+  // * create the post
+  // const { data, error } = await supabase
+  //   .from("post")
+  //   .insert([
+  //     {
+  //       user_id: userId,
+  //       caption,
+  //       published: media.length > 0 ? false : true,
+  //     },
+  //   ])
+  //   .select(
+  //     `*, user:profiles(id, name, username, profile_url), likes: like(user_id), comments: comment(user_id), reposts: repost(user_id), content(*)`
+  //   );
+  // if (error) return res.status(500).json({ error: error.message });
 
-  const { data, error } = await supabase
-    .from("post")
-    .insert([
-      {
-        user_id: userId,
-        caption,
-      },
-    ])
-    .select(
-      `*, user:profiles(id, name, username, profile_url), likes: like(user_id), comments: comment(user_id), reposts: repost(user_id), content(*)`
-    );
-  if (error) return res.status(500).json({ error: error.message });
+  // // pass caption, userId, and postId to the extractHashtags function
+  // extractHashtags(caption, userId, data[0].id);
 
-  // console.log(data[0]);
+  // // count the likes comments and resposts of the data
+  // data[0].isLiked = false;
+  // data[0].isReposted = false;
 
-  // count the likes comments and resposts of the data
-  data[0].isLiked = false;
-  data[0].isReposted = false;
+  // data[0].likes = data[0].likes.length;
+  // data[0].comments = data[0].comments.length;
+  // data[0].reposts = data[0].reposts.length;
 
-  // check if the user has liked the post
-  data[0].likes.forEach((like) => {
-    if (like.user_id === req.user.id) {
-      data[0].isLiked = true;
-    }
-  });
+  // if (!media || media.length === 0) return res.status(201).json(data);
 
-  // check if the user has reposted the post
-  data[0].reposts.forEach((repost) => {
-    if (repost.user_id === req.user.id) {
-      data[0].isReposted = true;
-    }
-  });
+  // // else if there is media files then send a resonse before creating the media
+  // res.status(201).json(data);
 
-  data[0].likes = data[0].likes.length;
-  data[0].comments = data[0].comments.length;
-  data[0].reposts = data[0].reposts.length;
-  // pass caption, userId, and postId to the extractHashtags function
-  extractHashtags(caption, userId, data[0].id);
+  //* create the media
+  // loop through the media files and check if the file is a video or image
+  try {
+    media.forEach(async (file, index) => {
+      if (file.mimetype.toString().startsWith("image")) {
+        let width = dimensions[index]?.width || 500,
+          height = dimensions[index]?.height || 500;
+        let buffer = file.buffer;
 
-  return res.status(201).json(data);
+        // if file is heic convert it to jpeg
+        if (
+          file.mimetype.toString() === "image/heic" ||
+          file.mimetype.toString() === "image/heif"
+        ) {
+          buffer = await convert({
+            buffer,
+            format: "JPEG",
+          });
+        }
+
+        // compress the image
+        await sharp(buffer)
+          .resize(2000)
+          .jpeg({ mozjpeg: true, quality: 88, force: false })
+          .png({ quality: 88, force: false })
+          .gif({ quality: 88, force: false })
+          .toBuffer({ resolveWithObject: true })
+          .then(({ data, info }) => {
+            console.log(info);
+            buffer = data;
+            width = info.width;
+            height = info.height;
+          })
+          .catch((err) => {});
+
+        console.log(width, height);
+        res.send("ok");
+        // create blurhash
+        // const { data: sharpBuffer, info } = await sharp(file.buffer)
+        //   .resize(40)
+        //   .ensureAlpha()
+        //   .raw()
+        //   .toBuffer({
+        //     resolveWithObject: true,
+        //   });
+
+        // const blurhash = encode(sharpBuffer, info.width, info.height, 3, 3);
+
+        // // create the media with type image
+        // const { data: media, error } = await supabase
+        //   .from("content")
+        //   .insert([
+        //     {
+        //       post_id: data[0].id,
+        //       type: "image",
+        //       width,
+        //       height,
+        //       blurhash,
+        //     },
+        //   ])
+        //   .select("id")
+        //   .single();
+        // console.log(error);
+        // if (error) return;
+
+        // axios
+        //   .put(
+        //     `https://ny.storage.bunnycdn.com/momenel/posts/${media.id}.${
+        //       file.mimetype.toString().split("/")[1]
+        //     }`,
+        //     file.buffer,
+        //     {
+        //       headers: {
+        //         AccessKey: "d915734c-bee2-4e6a-bb362bf9f500-edf0-4ba6",
+        //         "Content-Type": "application/octet-stream",
+        //       },
+        //     }
+        //   )
+        //   .then(async (response) => {
+        //     console.log(response.status);
+        //     // update status to published
+        //     await supabase
+        //       .from("content")
+        //       .update({ status: "published" })
+        //       .eq("id", media.id);
+        //   })
+        //   .catch((error) => {
+        //     console.log(error.message);
+        //   });
+      } else if (file.mimetype.toString().startsWith("video")) {
+        console.log("video");
+      } else {
+        console.log("other");
+      }
+    });
+  } catch (error) {
+    // //todo: set post status to error
+    // console.log(error.message);
+    console.log("error");
+  }
+
+  // // get all the content and if all the content is published then update the post status to published
+  // const { data: content, error: contentError } = await supabase
+  //   .from("content")
+  //   .select("status")
+  //   .eq("post_id", data[0].id);
+
+  // if (contentError) return;
+
+  // // check if all the content is published
+  // let isPublished = true;
+  // content.forEach((item) => {
+  //   if (item.status !== "published") isPublished = false;
+  // });
+
+  // // if all the content is published then update the post status to published
+  // if (isPublished) {
+  //   const { data: post, error: postError } = await supabase
+  //     .from("post")
+  //     .update({ published: true })
+  //     .eq("id", data[0].id);
+  //   if (postError) return;
+  // }
 };
 
 // PATCH /posts/:id
