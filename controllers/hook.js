224a98816881158ac80674fbf6f31ff469140315
farhan haider <fhaider085@gmail.com>
2023-06-18 16:47:46 -0400

    feat(create-post): webhook working
    - also made changes to create post to only update status if content is images only
    
    BREAKING CHANGE: farhan

--- controllers/hook.js
+++ controllers/hook.js
@@ -1,6 +1,48 @@
+import supabase from "../supabase/supabase.js";
+
 const videoHook = async (req, res) => {
-  console.log("video hook");
-  console.log(req.body); // Call your action on the request here
+  let { VideoGuid, Status } = req.body;
+  // set video status to ready if status is 3 or 4 and error if status is 5
+  if (Status == 3 || Status == 4) {
+    console.log("video ready");
+    // set post status to ready if all post videos are processed
+    let { data, error } = await supabase
+      .from("content")
+      .update({ status: "published" })
+      .select("post_id")
+      .eq("id", VideoGuid)
+      .single();
+
+    // check if all content for post is ready
+    const { data: content, error: contentError } = await supabase
+      .from("content")
+      .select("status")
+      .eq("post_id", data.post_id);
+
+    if (contentError) return;
+
+    let isPublished = true;
+    content.forEach((item) => {
+      if (item.status !== "published") isPublished = false;
+    });
+
+    // if all the content is published then update the post status to published
+    if (isPublished) {
+      const { data: post, error: postError } = await supabase
+        .from("post")
+        .update({ published: true })
+        .eq("id", data.post_id);
+      if (postError) return;
+    }
+  } else if (Status == 5) {
+    await supabase
+      .from("content")
+      .update({ status: "error" })
+      .eq("id", VideoGuid);
+
+    //todo: send error notification
+  }
+
   res.status(200).end(); // Responding is important
 };
 
