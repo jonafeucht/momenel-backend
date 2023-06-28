f2618e28a84b51f6e50d9d092de795a7e11b213d
farhan haider <fhaider085@gmail.com>
2023-06-28 14:43:25 -0400

    search feed done

--- controllers/search.js
+++ controllers/search.js
@@ -62,4 +62,69 @@ const getSearchSuggestions = async (req, res) => {
   }
 };
 
-export { getSearchSuggestions };
+const searchFeed = async (req, res) => {
+  const { id: userId } = req.user;
+  let { from, to, hashtag } = req.params;
+  console.log(from, to, hashtag);
+  let hashtagId;
+  const querySQL = `id,post!inner(id,caption,user_id,created_at, user:profiles(name,username,profile_url), likes: like(count), comments: comment(count), reposts: repost(count), content(id,type,width,height,blurhash,format))`;
+
+  // get hashtag id
+  let { data: data1, error: error1 } = await supabase
+    .from("hashtag")
+    .select("id")
+    .eq("hashtag", hashtag)
+    .limit(1);
+
+  if (error1) {
+    console.log(error1);
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+
+  if (data1.length === 0) {
+    return res.status(404).json({ error: "Hashtag not found" });
+  }
+
+  hashtagId = data1[0].id;
+
+  let { data: data23, error: error3 } = await supabase
+    .from("post_hashtags")
+    .select(querySQL)
+    .eq("post.published", true)
+    .eq("hashtag_id", hashtagId)
+    .order("created_at", { ascending: false })
+    .range(from, to)
+    .limit(10);
+
+  if (error3) {
+    console.log(error3);
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+
+  // remove all dubplicates
+  data23 = data23.filter(
+    (post, index, self) => index === self.findIndex((t) => t.id === post.id)
+  );
+
+  // get is user is following the hashtag
+  let { data: data2, error: error2 } = await supabase
+    .from("user_hashtag")
+    .select("hashtag_id")
+    .eq("user_id", userId)
+    .eq("hashtag_id", hashtagId);
+
+  if (error2) {
+    console.log(error2);
+    return res.status(500).json({ error: "Something went wrong" });
+  }
+
+  let resData = {
+    hashtagId: hashtagId,
+    isFollowing: data2.length > 0,
+    posts: data23,
+  };
+
+  return res.send(resData);
+};
+
+export { getSearchSuggestions, searchFeed };
