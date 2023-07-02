ce3fe908d4cb1e7f1b756bdcd45b8abf75581761
farhan haider <fhaider085@gmail.com>
2023-07-02 15:32:53 -0400

    notfication updated

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -4,14 +4,14 @@ const getNotification = async (req, res) => {
   const { id: userId } = req.user;
   const { from, to } = req.params;
   // get all notifications for a user from the database
-  console.log(from, to);
+
   let { data: notifications, error } = await supabase
     .from("notifications ")
     .select(
-      "*, user:profiles!notifications_sender_id_fkey(id,username,profile_url),comment(id,post_id),post(id,caption,user_id,created_at, user:profiles(name,username,profile_url), likes: like(count), comments: comment(count), reposts: repost(count), content(id,type,width,height,blurhash,format))"
+      "*, user:profiles!notifications_sender_id_fkey(id,username,profile_url),comment(id,post_id),post(id)"
     )
     .eq("receiver_id", userId)
-    .order("created_at", { ascending: false }) //! keep this ascending true (not a bug)
+    .order("created_at", { ascending: false })
     .range(from, to)
     .limit(10);
 
