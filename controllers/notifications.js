f7ddcd7463b16ac7d35b9e6626d4617aa176e17a
farhan haider <fhaider085@gmail.com>
2023-07-02 13:31:31 -0400

    notifications limit added

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -11,9 +11,9 @@ const getNotification = async (req, res) => {
       "*, user:profiles!notifications_sender_id_fkey(id,username,profile_url),comment(id,post_id),post(id,caption,user_id,created_at, user:profiles(name,username,profile_url), likes: like(count), comments: comment(count), reposts: repost(count), content(id,type,width,height,blurhash,format))"
     )
     .eq("receiver_id", userId)
-    .order("created_at", { ascending: true }) //! keep this ascending true (not a bug)
-    .range(from, to);
-  // .limit(10);
+    .order("created_at", { ascending: false }) //! keep this ascending true (not a bug)
+    .range(from, to)
+    .limit(10);
 
   if (error) {
     console.log(error);
