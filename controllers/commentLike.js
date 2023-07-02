05f6f34aa8528198a619d4f90e80589ecc66ea2f
farhan haider <fhaider085@gmail.com>
2023-07-02 11:47:37 -0400

    comment like notification

--- controllers/commentLike.js
+++ controllers/commentLike.js
@@ -20,14 +20,39 @@ const handleLikeComment = async (req, res) => {
       .eq("comment_id", commentId)
       .eq("user_id", userId);
     if (error) return res.status(500).json({ error: "Something went wrong" });
+
+    // remove like notification
+    const { data: noti, error: e4 } = await supabase
+      .from("notifications")
+      .delete()
+      .eq("sender_id", userId)
+      .eq("comment_id", commentId)
+      .eq("type", "comment_like");
+
     return res.status(204).send();
   } else {
-    console.log("user has not liked this post");
     // user has not liked this post, so add like
     const { data, error } = await supabase
       .from("comment_likes")
-      .insert([{ comment_id: commentId, user_id: userId }]);
+      .insert([{ comment_id: commentId, user_id: userId }])
+      .select("comment(post(user_id))")
+      .single();
     if (error) return res.status(500).json({ error: "Something went wrong" });
+    // send like notification
+    if (data.comment.post.user_id !== userId) {
+      const { data: data3, error: error3 } = await supabase
+        .from("notifications")
+        .insert([
+          {
+            sender_id: userId,
+            receiver_id: data.comment.post.user_id,
+            type: "comment_like",
+            isRead: false,
+            comment_id: commentId,
+          },
+        ]);
+    }
+
     return res.status(201).send();
   }
 };
