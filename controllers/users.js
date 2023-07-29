4b64f886a418504cdcc1731243770fcfd9872cd5
farhan haider <fhaider085@gmail.com>
2023-07-29 10:45:36 -0400

    fix(edit-profile): regex updated

--- controllers/users.js
+++ controllers/users.js
@@ -194,10 +194,11 @@ const updateEditProfile = async (req, res) => {
     website.length > 0 &&
     !website.match(
       "^(https?:\\/\\/)?" + // protocol
-        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-        "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-        "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-        "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+        "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name and extension
+        "localhost|" + // localhost
+        "(\\d{1,3}\\.){3}\\d{1,3})" + // OR ip (v4) address
+        "(\\:\\d+)?(\\/[-a-z\\d%@_.~+&:]*)*" + // port and path
+        "(\\?[;&a-z\\d%@_.,~+&:=-]*)?" + // query string
         "(\\#[-a-z\\d_]*)?$",
       "i"
     )
